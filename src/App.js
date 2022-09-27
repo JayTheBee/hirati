313cd7beb6b9ce4825f9c7f9cde0b71a912d4bca
JayTheBee <jediboy.betos811@gmail.com>
2022-09-27 17:27:31 +0800

    Edited label to check for functioning CI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hirati CI/CD and Initial Reactjs app
         </p>
         <a
           className="App-link"
