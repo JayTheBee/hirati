7c25fec0807db70b8170482e1e9252f95bbba7bd
JayTheBee <jediboy.betos811@gmail.com>
2022-12-03 17:27:10 +0800

    Refactored and fixed editor

--- client/src/pages/Editor/index.js
+++ client/src/pages/Editor/index.js
@@ -3,28 +3,40 @@ import {Buffer} from 'buffer';
 import  MonacoEditor from "@monaco-editor/react";
 import Select from "react-select";
 
-// import { loadLanguage, langNames, langs } from '@uiw/codemirror-extensions-langs';
+const OutputDetails = ({output}) => {
+  return(
+    <>
+      <h4>Console Output:</h4>
+      <i>{output.stdout}</i>
+      <p><b> Status:</b> {output.status.description}</p>
+      <p><b>Runtime memory is </b>{output.memory} kb</p>
+      <p><b>Runtime is</b> {output.time} seconds</p>
+    </>
+  )
 
+
+}
 
 const CodeEditor = () => {
   const [code, setCode] = useState("");
-  const [error, setError] = useState(null)
-  const [lang, setLang] = useState({name:"python",langId:71})
+  const [lang, setLang] = useState({ name:"javascript", id:63 })
+  const [processing, setProcessing] = useState(null)
+  const [output, setOutput] = useState(null)
+  const supportedLang = [{ name:"c", id:50 }, { name:"cpp", id:76 }, { name:"java", id:62 }, { name:"python", id:71 }, { name:"shell", id:46 }, { name:"javascript", id:63 }]
 
- const supportedLang = [{ name:"cpp", langId:76 }, { name:"java", langId:62 }, { name:"python", langId:71 }, { name:"shell", langId:46 }, { name:"javascript", langId:63 }]
-
-  const handleSubmit = async(e) => {
+  const handleCompile = async(e) => {
     e.preventDefault()
+    setProcessing(true)
     console.log("lang is ", lang)
     console.log("code is ",code)
     const encodedSource = Buffer.from(code).toString('base64')
     console.log("code64 is ", encodedSource)
 
-    const response = await fetch('http://192.168.100.219:2358/submissions/?base64_encoded=true',{
+    const compileResponse = await fetch('http://judge.hirati.app/submissions/?base64_encoded=true',{
       method: 'POST',
 		  body: JSON.stringify({
         source_code: encodedSource,
-        language_id: lang.langId
+        language_id: lang.id
       }),
       headers: {
         accept: "application/json",
@@ -32,50 +44,69 @@ const CodeEditor = () => {
       }
     })
 		
-		const json = await response.json()
+		const tokenJson = await compileResponse.json()
 
-    if (!response.ok) {
-		  setError(json.error)
-		  console.log("err response is ", error)
+    if (!compileResponse.ok) {
+      setProcessing(false)
+		  console.log("err response is ", tokenJson.error)
 		}
-		if (response.ok) {
-		  setError(null)
-		  console.log('submission token is:', json)
-      const tokenresp = await fetch("http://192.168.100.219:2358/submissions/"+ json.token , {
+		if (compileResponse.ok) {
+		  console.log('submission token is: ', tokenJson)
+      checkStatus(tokenJson.token)
+    }
+  }
+
+  const checkStatus = async (token) => {
+    try{
+      let statusResponse = await fetch("http://judge.hirati.app/submissions/"+ token , {
         method: 'GET',
         headers: {
           "Content-Type": "application/json"
         }
-      })
-      const tokenrespjson = await tokenresp.json()
-      console.log("tokenrespjson is ", tokenrespjson)
+      }) 
+      console.log("status resp is ", statusResponse)
+      const statusRespJson = await statusResponse.json()
+      console.log("statusrespjson is ", statusRespJson)
+      let statusId = statusRespJson.status.id 
+      
+      if (statusId === 1 || statusId === 2) {
+        // still processing
+        setTimeout(() => {
+          checkStatus(token);
+        }, 3000);
+        return;
+      } else {
+        setProcessing(false)
+        setOutput(statusRespJson)
+        console.log("it fuckin works. resp is ", statusRespJson)
+        return;
+      }      
+
+    }catch(err){
+      setProcessing(false)
+      console.log("checkstatus err is", err)
     }
+
   }
+  
 
 
   return (
     <>
-      <form className="create-code" onSubmit={handleSubmit}> 
+      <form className="create-code" onSubmit={handleCompile}> 
         <h2>CODEMIRROR EDITOR COMPONENT</h2>
-        {/* <select  value={lang} onChange={handleLangChange}>
-          <option value='{ name:"cpp", langId:76 }'>cpp</option>
-          <option value='{ name:"java", langId:62 }'>java</option>
-          <option value='{ name:"python", langId:71 }'>python</option>
-          <option value='{ name:"shell", langId:46 }'>bash</option>
-          <option value='{ name:"javascript", langId:63 }'>javascript</option>
-        </select> */}
         <Select
           name="languages"
           options={supportedLang}
           value={lang}
           onChange={setLang}
           getOptionLabel={(option) => option.name}
-          getOptionValue={(option) => option.langId} 
+          getOptionValue={(option) => option.id} 
         />
         <br />
-        <button  type="submit">send code</button>
+        <button  type="submit">{processing ? "Processing..." : "Compile and Execute"}</button>
         <MonacoEditor
-          height="80vh"
+          height="50vh"
           language={lang.name}
           defaultLanguage="javascript"
           defaultValue="// some comment"
@@ -83,6 +114,7 @@ const CodeEditor = () => {
           onChange={(value) => setCode(value)}
           theme="vs-dark"
         />
+        {output && <OutputDetails output={output}/>}
         <br />
       </form>
     </>
