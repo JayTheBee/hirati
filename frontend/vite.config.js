94b226a9b240a36c147925759004a676c5bdd5bb
JayTheBee <jediboy.betos811@gmail.com>
2023-03-22 19:16:17 +0800

    Minor fixes, docker, env fix, tree structure updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
 /* eslint-disable */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv  } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+export default ( { mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: parseInt(process.env.VITE_PORT),
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BACKEND,
+        },
       },
     },
-  },
-});
+  })
+}
