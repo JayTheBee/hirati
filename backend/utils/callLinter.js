7efb0d328201e6b18f41aa523a0e957d23379c3f
JayTheBee <jediboy.betos811@gmail.com>
2023-04-18 23:53:41 +0800

    formatted linter calls

--- backend/utils/callLinter.js
+++ backend/utils/callLinter.js
@@ -19,11 +19,20 @@ export const lintC = async (req, res) => {
     const {stdout, stderr, err } = await exec_util("python -m cpplint --filter=-legal/copyright,-whitespace --quiet " + tmpFilePath) 
     console.log("stderr is ", stderr)
     console.log("stdout is ", stdout)
+    console.log("err is ", err)
     return res.status(200).send('console log is ' + stdout)
   } catch (error) {
-    console.log("stderr is ", error.stderr)
-    console.log("stdout is ", error.stdout)
-    return res.status(200).send('console log is ' + error.stdout)
+
+    console.log("Error stdout is ", error.stdout)
+    let errArr = error.stderr.split('\n')
+    errArr = errArr.filter(s=>~s.indexOf("code.c"));
+    errArr.forEach((element, index) => {
+      errArr[index] = element.substring(element.indexOf("code.c"))
+    });
+    console.log("Formatted Error Array is ", errArr)
+
+
+    return res.status(200).send(errArr)
     
   }
 }
@@ -35,13 +44,29 @@ export const lintJava = async (req, res) => {
   fs.writeFileSync(tmpFilePath, req.body.code);
 
   try {
-    const {stdout, stderr, err } = await exec_util("java -jar ../../utils/linters/checkstyle.jar /google_checks.xml " + tmpFilePath) 
+    const {stdout, stderr, err } = await exec_util("java -jar ../utils/linters/checkstyle.jar -c /google_checks.xml " + tmpFilePath) 
     console.log("stderr is ", stderr)
     console.log("stdout is ", stdout)
-    return res.status(200).send('console log is ' + stdout)
+
+    let lintArr = stdout.split('\n')
+    lintArr = lintArr.filter(s=>~s.indexOf(".java"));
+    lintArr.forEach((element, index) => {
+      lintArr[index] = element.substring(element.indexOf(".java"))
+    });
+    console.log("Formatted Linter Array is ", lintArr)
+
+    return res.status(200).send('console log is ' + lintArr)
   } catch (error) {
-    console.log("stderr is ", error.stderr)
-    console.log("stdout is ", error.stdout)
+    console.log("Error stderr is ", error.stderr)
+    console.log("Error stdout is ", error.stdout)
+    let errArr = error.stderr.split('\n')
+    errArr = errArr.filter(s=>~s.indexOf(".java"));
+    errArr.forEach((element, index) => {
+      errArr[index] = element.substring(element.indexOf(".java"))
+    });
+    console.log("Formatted Error Array is ", errArr)
+
+
     return res.status(200).send('console log is ' + error.stdout)
     
   }
@@ -59,9 +84,14 @@ export const lintPython = async (req, res) => {
     console.log("stdout is ", stdout)
     return res.status(200).send('console log is ' + stdout)
   } catch (error) {
-    console.log("stderr is ", error.stderr)
-    console.log("stdout is ", error.stdout)
-    return res.status(200).send('console log is ' + error.stdout)
+
+    const errArr = error.stdout.split('\n')
+    errArr.forEach((element, index) => {
+      errArr[index] = element.substring(element.indexOf("code.py"))
+    });
+    console.log("Formatted Error Array is ", errArr)
+
+    return res.status(200).send(errArr)
   }
 }
 
@@ -77,8 +107,15 @@ export const lintCPP = async (req, res) => {
     console.log("stdout is ", stdout)
     return res.status(200).send('console log is ' + stdout)
   } catch (error) {
-    console.log("stderr is ", error.stderr)
-    console.log("stdout is ", error.stdout)
-    return res.status(200).send('console log is ' + error.stdout)
+
+    console.log("Error stdout is ", error.stdout)
+    let errArr = error.stderr.split('\n')
+    errArr = errArr.filter(s=>~s.indexOf("code.c"));
+    errArr.forEach((element, index) => {
+      errArr[index] = element.substring(element.indexOf("code.c"))
+    });
+    console.log("Formatted Error Array is ", errArr)
+
+    return res.status(200).send(errArr)
   }
 }
