39d5b79e9be708b1d51d8b501423a6e1fa010fc0
JayTheBee <jediboy.betos811@gmail.com>
2023-05-12 01:22:16 +0800

    User time stamp and constant formatting

--- frontend/src/constants/python-constructs.jsx
+++ frontend/src/constants/python-constructs.jsx
@@ -1,31 +1,87 @@
 const reservedPython = [
-  { id: 1, value: 'if', category: 'control flow' },
-  { id: 2, value: 'elif', category: 'control flow' },
-  { id: 3, value: 'else', category: 'control flow' },
-  { id: 4, value: 'while', category: 'control flow' },
-  { id: 5, value: 'for', category: 'control flow' },
-  { id: 6, value: 'break', category: 'control flow' },
-  { id: 7, value: 'continue', category: 'control flow' },
-  { id: 8, value: 'and', category: 'control flow' },
-  { id: 9, value: 'or', category: 'control flow' },
-  { id: 10, value: 'not', category: 'control flow' },
-  { id: 11, value: 'def', category: 'function' },
-  { id: 12, value: 'return', category: 'function' },
-  { id: 13, value: 'yield', category: 'function' },
-  { id: 14, value: 'class', category: 'object oriented' },
-  { id: 15, value: 'try', category: 'exception handling' },
-  { id: 16, value: 'except', category: 'exception handling' },
-  { id: 17, value: 'finally', category: 'exception handling' },
-  { id: 18, value: 'raise', category: 'exception handling' },
-  { id: 19, value: 'assert', category: 'exception handling' },
-  { id: 20, value: 'import', category: 'modules' },
-  { id: 21, value: 'from', category: 'modules' },
-  { id: 22, value: 'as', category: 'modules' },
-  { id: 23, value: 'global', category: 'modules' },
-  { id: 24, value: 'nonlocal', category: 'modules' },
-  { id: 25, value: 'None', category: 'others' },
-  { id: 26, value: 'in', category: 'others' },
-  { id: 27, value: 'is', category: 'others' },
-  { id: 28, value: 'lambda', category: 'others' }];
+  {
+    id: 1, value: 'if', label: 'if', category: 'control flow',
+  },
+  {
+    id: 2, value: 'elif', label: 'elif', category: 'control flow',
+  },
+  {
+    id: 3, value: 'else', label: 'else', category: 'control flow',
+  },
+  {
+    id: 4, value: 'while', label: 'while', category: 'control flow',
+  },
+  {
+    id: 5, value: 'for', label: 'for', category: 'control flow',
+  },
+  {
+    id: 6, value: 'break', label: 'break', category: 'control flow',
+  },
+  {
+    id: 7, value: 'continue', label: 'continue', category: 'control flow',
+  },
+  {
+    id: 8, value: 'and', label: 'and', category: 'control flow',
+  },
+  {
+    id: 9, value: 'or', label: 'or', category: 'control flow',
+  },
+  {
+    id: 10, value: 'not', label: 'not', category: 'control flow',
+  },
+  {
+    id: 11, value: 'def', label: 'def', category: 'function',
+  },
+  {
+    id: 12, value: 'return', label: 'return', category: 'function',
+  },
+  {
+    id: 13, value: 'yield', label: 'yield', category: 'function',
+  },
+  {
+    id: 14, value: 'class', label: 'class', category: 'object oriented',
+  },
+  {
+    id: 15, value: 'try', label: 'try', category: 'exception handling',
+  },
+  {
+    id: 16, value: 'except', label: 'except', category: 'exception handling',
+  },
+  {
+    id: 17, value: 'finally', label: 'finally', category: 'exception handling',
+  },
+  {
+    id: 18, value: 'raise', label: 'raise', category: 'exception handling',
+  },
+  {
+    id: 19, value: 'assert', label: 'assert', category: 'exception handling',
+  },
+  {
+    id: 20, value: 'import', label: 'import', category: 'modules',
+  },
+  {
+    id: 21, value: 'from', label: 'from', category: 'modules',
+  },
+  {
+    id: 22, value: 'as', label: 'as', category: 'modules',
+  },
+  {
+    id: 23, value: 'global', label: 'global', category: 'modules',
+  },
+  {
+    id: 24, value: 'nonlocal', label: 'nonlocal', category: 'modules',
+  },
+  {
+    id: 25, value: 'None', label: 'None', category: 'others',
+  },
+  {
+    id: 26, value: 'in', label: 'in', category: 'others',
+  },
+  {
+    id: 27, value: 'is', label: 'is', category: 'others',
+  },
+  {
+    id: 28, value: 'lambda', label: 'lambda', category: 'others',
+  }];
 
 export default reservedPython;
